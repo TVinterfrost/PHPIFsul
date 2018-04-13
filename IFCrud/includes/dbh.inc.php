fa7ec20bad4c2bd0f98c048e60b2490bce8310da
T <tvinterfrost@live.com>
2018-04-12 21:07:09 -0300

    Add files via upload

---
+++ IFCrud/includes/dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServerName = "127.0.0.1";
+$dbUserName = "root";
+$dbPassword = "testedb1";
+$dbName = "loginsystem";
+
+$conn = mysqli_connect($dbServerName, $dbUserName, $dbPassword, $dbName);
